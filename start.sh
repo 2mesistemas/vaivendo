8f2da13dcc2d7fce000779dd5d7067637187e1f2
2mesistemas <90299970+2mesistemas@users.noreply.github.com>
2022-07-18 19:19:12 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,11 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install openjdk-11-jre openjdk-11-jdk -y
 sudo apt install snapd -y
 sudo snap install teams
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install google-chrome-stable -y
+sudo snap install brave
+#sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#sudo apt-get update
+#sudo apt-get install google-chrome-stable -y
 #sudo snap install chromium
 
 # Client RDP Muito bom
